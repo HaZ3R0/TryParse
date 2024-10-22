c77452d22cf73c4a678c876604c595c17061cbc0
HaZ3R0 <megamackan@outlook.com>
2024-10-22 16:15:24 +0200

    Klar

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Reflection.Metadata;
+int resultat;
+
+if (!TryParse(Console.ReadLine(), out resultat)){
+    Console.WriteLine("Skriv in ett heltal");
+}
+
+static bool TryParse(string s, out int result){
+    
+    try{
+        result = int.Parse(s);
+        return true;
+    }
+
+    catch{
+        result = 0;
+        return false;
+    }
+
+}
+
+
+
+
+
+
